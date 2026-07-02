9b7b7e12c8e61465dd41dd2a7a144c714fdc6cea
415-15-Chongpon <54615@yupparaj.ac.th>
2026-07-02 08:42:59 +0700

    digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("My Digital Profile")
+st.header("เเนะนําตัว")
+st.write("นายทรงพล พญาตัง")
+st.write("ซื่อเล่น ไอคิว")
+st.write("เลขที่ 15 ม.4/15")
+st.header("เป้าหมายเเละสิ่งที่อยากทําในอนาคต")
+st.write("เป้าหมาย: **สร้างคอมมูนิตี้เกมเล็ก ของตัวเอง")
+st.write("สิ่งที่อยากเป็นในอนาคต:นักสร้างสรรค์คอนเทนต์ (content creator)")
